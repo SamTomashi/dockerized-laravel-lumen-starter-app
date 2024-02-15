c04f75e3a1cee6cfa821f90f7169d701d1c9690b
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-02-15 12:03:48 +0300

    making mysql the default connection

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default' => 'sqlsrv',
+    'default' => 'mysql',
         'connections' => [
             'sqlsrv' => [
                 'driver' => 'sqlsrv',
@@ -14,10 +14,18 @@ return [
             ],
             'mysql' => [
                 'driver' => 'mysql',
+
+                // 'host' => env('DB_HOST'),
+                // 'database' => env('DB_DATABASE'),
+                // 'username' => env('DB_USERNAME'),
+                // 'password' => env('DB_PASSWORD'),
+
                 'host' => env('DB_HOST'),
+                'port' => env('DB_PORT'),
                 'database' => env('DB_DATABASE'),
                 'username' => env('DB_USERNAME'),
                 'password' => env('DB_PASSWORD'),
+
                 'charset'   => 'utf8',
                 'collation' => 'utf8_unicode_ci',
             ],
